5faaf22188543e0205c19de1f22e5f1d2b9c1e09
Mounikaullengula <44670361+Mounikaullengula@users.noreply.github.com>
2018-11-05 10:01:38 +0530

    Add files via upload

---
+++ Contact.java
@@ -0,0 +1,35 @@
+package com.agilecrm.dto;
+
+public class Contact {
+	
+		private int id;
+		private String fname;
+		private String lname;
+		private String mail;
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getFname() {
+			return fname;
+		}
+		public void setFname(String fname) {
+			this.fname = fname;
+		}
+		public String getLname() {
+			return lname;
+		}
+		public void setLname(String lname) {
+			this.lname = lname;
+		}
+		public String getMail() {
+			return mail;
+		}
+		public void setMail(String mail) {
+			this.mail = mail;
+		}
+	
+}
+
